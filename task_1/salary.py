0ed7cbe9bd714fb69737daf3c769dd37574b3d26
PoppyHanna <142433975+PoppyHanna@users.noreply.github.com>
2025-06-22 19:45:38 -0400

    up task 1

--- task_1/salary.py
+++ task_1/salary.py
@@ -5,19 +5,27 @@ def total_salary(path):
     total = 0
     count = 0
 
-    with open(path, 'r', encoding='utf-8') as file:
-        for line in file:
-            parts = line.strip().split(',')
-            if len(parts) == 2:
-                try:
-                    salary = float(parts[1])
-                    total += salary
-                    count += 1
-                except ValueError:
-                    continue    
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                parts = line.strip().split(',')
+                if len(parts) == 2:
+                    try:
+                        salary = float(parts[1])
+                        total += salary
+                        count += 1
+                    except ValueError:
+                        continue
+    except FileNotFoundError:
+        print(f"Файл '{path}' не знайдено")
+        return 0, 0
+    except Exception as e:
+        print(f"Сталася помилка при читанні файлу: {e}")
+        return 0, 0
+    
     if count == 0:
         return 0, 0
     average = total / count
-    return round(total), round(average)           
+    return round(total), round(average)
 total, average = total_salary("salaries.txt")
 print(f"Загальна сума: {total}, Середня: {average}")
